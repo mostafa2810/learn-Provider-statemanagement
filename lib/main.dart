570afd74ac314bcf81ec2da711ab680a4b4a2dc6
unknown <mostafa.helaly2810@gmail.com>
2022-04-29 14:11:50 +0200

    why selectors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+/* part2 before Selector:
+     first of all excuse my english grammar|---Egyption bro------| 
+     you might notice that if you press the button1
+     the text 1 changed and that because of the rebuild of
+     the consumer but the problem here is that when you press the
+     the button1 all of 2 buttons and text have been rebuilt
+     even if the only change you can see is the text 1 change  and 
+     that is because the type of all consumers is<Model>
+     so to have more control over the rebuild you want to happen
+     we will use the selectors
+
+     */
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -23,37 +35,55 @@ class _MyAppState extends State<MyApp> {
     return ChangeNotifierProvider(
       create: (BuildContext context) => Model(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           appBar: AppBar(
             title: Text('Learn Providers'),
           ),
-
-          /*notice that you have to specify the type of the
+          body: Column(
+            children: [
+              /*notice that you have to specify the type of the
          consumer (<Model> in this example)
          requierd builder: have to Take 3 parameters
            1-context
            2-anonymous object of the class  (<value> in this example)
            3-the return widget
         */
-          body: Consumer<Model>(
-            builder: (BuildContext context, value, child) {
-              return Column(
-                children: [
-                  Center(child: Text(value.name)),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  MaterialButton(
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    onPressed: () {
-                      value.changeName();
-                    },
-                    child: const Text("Do somthing here"),
-                  ),
-                ],
-              );
-            },
+              Consumer<Model>(
+                builder: (BuildContext context, value, child) {
+                  return Column(
+                    children: [
+                      Center(child: Text(value.doSomthing1)),
+                      MaterialButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          value.changeName1();
+                        },
+                        child: const Text("My name is"),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              Consumer<Model>(
+                builder: (BuildContext context, value, child) {
+                  return Column(
+                    children: [
+                      Center(child: Text(value.doSomthing2)),
+                      MaterialButton(
+                        color: Colors.blue,
+                        textColor: Colors.white,
+                        onPressed: () {
+                          value.changeName2();
+                        },
+                        child: const Text("My Email is:"),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
@@ -61,13 +91,18 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class Model extends ChangeNotifier {
-  String name = 'Welcome';
-  changeName() {
-    name = 'Mostafa';
+class Model with ChangeNotifier {
+  String doSomthing1 = 'Welcome';
+  String doSomthing2 = 'Welcome';
+  changeName1() {
+    doSomthing1 = 'Mostafa Helaly';
     notifyListeners();
-     //to rebuild the consumer that consume to the same class
+    //to rebuild the consumer that consume to the same class
+  }
 
-    
+  changeName2() {
+    doSomthing2 = 'mostafa.helaly2810@gmail.com';
+    notifyListeners();
+    //to rebuild the consumer that consume to the same class
   }
 }
